2550854fb3691b8e0ee15d8efa9cd71bfcfd0ec7
khlndaaa <khlanovskiy@ukr.net>
2026-06-17 14:24:10 +0300

    Update DeployChad.sol

--- src/DeployChad.sol
+++ src/DeployChad.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title DeployChad
+ * @notice ERC20 token deployed on Base
+ * @dev DeployChad (DC) - Base Guild / Builder meme
+ */
+contract MyToken is ERC20 {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 totalSupply_
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, totalSupply_);
+    }
+}
